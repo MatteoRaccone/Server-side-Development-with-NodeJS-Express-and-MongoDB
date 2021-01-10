dc9b1163d480fe5a94ceb41b4e5bbe4fb0a37391
matteoraccone <matteo.raccone@gmail.com>
2021-01-10 12:41:33 +0100

    passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -34,27 +36,21 @@ app.set("view engine", "jade");
 app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-if(!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
+    next(err);
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+        next();
   }
-}
 }
 
 app.use(auth);
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
