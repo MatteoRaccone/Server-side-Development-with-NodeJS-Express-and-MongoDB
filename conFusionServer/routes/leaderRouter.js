c3ba9403bbf5a189db18b5ff165cc7995e700e74
matteoraccone <matteo.raccone@gmail.com>
2021-01-10 14:10:25 +0100

    mongoose population & assignment 3

--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const authenticate = require('../authenticate');
 
 const Leaders = require("../models/leaders");
 
--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const authenticate = require('../authenticate');
 
 const Promotions = require("../models/promotions");
 
