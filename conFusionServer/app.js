ef049f55badbcef38b49b4b28b34eb5e65b9832b
matteoraccone <matteo.raccone@gmail.com>
2021-01-16 15:15:54 +0100

    Integrating Client and Server

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,88 +1,123 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
-var passport = require('passport');
-var authenticate = require('./authenticate');
-var config = require('./config');
-const uploadRouter = require('./routes/uploadRouter');
+const express       = require('express');       
+const path          = require('path');
+const favicon       = require('serve-favicon');
+const logger        = require('morgan');
+const bodyParser    = require('body-parser');
+const session       = require('express-session');
+const passport      = require('passport');
+const authenticate  = require('./authenticate');
+const FileStore = require('session-file-store')(session);
+const cookieParser = require('cookie-parser');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const config = require('./config');
 
-var dishRouter = require("./routes/dishRouter");
-var promoRouter = require("./routes/promoRouter");
-var leaderRouter = require("./routes/leaderRouter");
+const index          = require('./routes/index');
+const users          = require('./routes/users');
+const dishRouter     = require('./routes/dishRouter');
+const promoRouter    = require('./routes/promoRouter');
+const leaderRouter   = require('./routes/leaderRouter');
+const uploadRouter   = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
-const mongoose = require("mongoose");
-const url = config.mongoUrl;
-const connect = mongoose.connect(url);
+const mongoose      = require('mongoose');
+mongoose.Promise    = require('bluebird');
+const url           = config.mongoUrl;
+const options       = config.mongoOpts;
+const connect = mongoose.connect(url, {
+  useMongoClient: true,
+  /* other options */
+});
+
 connect.then(
   db => {
-    console.log("Connected correctly to server");
+    console.log("\n Connected correctly to server!\n\n Output log: \n");
   },
   err => {
     console.log(err);
   }
 );
 
-var app = express();
+// Application
+const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(cookieParser('12345-67890-09876-54321'));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/imageUpload',uploadRouter);
-app.use("/dishes", dishRouter);
-app.use("/promotions", promoRouter);
-app.use("/leaders", leaderRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Secure traffic only
+app.all('*', (req, res, next) => {
+    if (req.secure) return next();
+    else res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+    //res.redirect(307, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
 });
 
-// error handler
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use( logger('dev'));
+app.use( bodyParser.json());
+app.use( bodyParser.urlencoded({ extended: false }));
+app.use( express.static( path.join(__dirname, 'public')));
+
+// Authentication Middleware
+app.use( passport.initialize());
+// app.use(passport.session());
+
+// Mount Routes
+app.use('/', index);
+app.use('/users', users);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+app.use('/favorite', favoriteRouter);
+
+/*function auth (req, res, next) {
+  console.log(req.user);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');                          
+    err.status = 401;
+    next(err);
+  }
+  else {
+      next();
+  }
+}
+app.use(auth);*/
+
+// Catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+
+// Development error handler: will print stacktrace.
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  // next is the last argument in above function
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
 });
 
-// Secure traffic only
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-  }
-});
-
+// Export module
 module.exports = app;
