b917331db93349d3e41d00e736c8922f261bc206
matteoraccone <matteo.raccone@gmail.com>
2021-01-16 12:37:25 +0100

    passport facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '228544332112188',
+        clientSecret: '1a8bb5969fd0dc36ec219b8a9719f79b'
+    }
 }
